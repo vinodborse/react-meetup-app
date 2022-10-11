79a0475a1e81b1eae89ba01c05a12ca52c14c3cb
vinod <borse002@gmail.com>
2022-10-11 13:22:57 +0530

    Added basic form

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,31 @@
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+function NewMeetupForm() {
+  return (
+    <Card>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Meetup Address</label>
+          <input type="text" required id="address" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Meetup Description</label>
+          <textarea required id="description" rows='5'/>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
