6b2903cda3ac268a8ef0a7b8431127672520823c
vinod <borse002@gmail.com>
2022-10-11 12:30:11 +0530

    Added wrapper comp and pass the data from it using props-children

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -2,25 +2,24 @@ import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-    return (
+  return (
     <header className={classes.header}>
-        <div className={classes.logo}>React Meetup</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to={'/'}>All Meetups</Link>
-                </li>
-                <li>
-                    <Link to={'/new-meetup'}>New Meetups</Link>
-                </li>
-                <li>
-                    <Link to={'/favoraties'}>Favoraties</Link>
-                </li>
-
-            </ul>
-        </nav>
+      <div className={classes.logo}>React Meetup</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}>All Meetups</Link>
+          </li>
+          <li>
+            <Link to={"/new-meetup"}>New Meetups</Link>
+          </li>
+          <li>
+            <Link to={"/favoraties"}>Favoraties</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-    );
+  );
 }
 
 export default MainNavigation;
